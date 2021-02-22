3d9306cf317cd92ec0978d2d5ba82805c22ad4e8
Bookaso <lohamat.s@gmail.com>
2021-02-22 22:41:04 +0700

    Level 3 added hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSChema = new mongoose.Schema ({
 });
 
 
-userSChema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSChema);
 
 app.get("/",function(req,res){
@@ -33,7 +31,7 @@ app.route("/login")
 })
 .post(function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
     User.findOne({email: username},function(err,foundUser){
       if (err) {
         console.log(err);
@@ -58,7 +56,7 @@ app.route("/register")
 .post(function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save((err)=>{
     if (err) {
